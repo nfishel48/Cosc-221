193d21332024589a10a14634c9d848c5c6d1fa09
Nathan Fishel <50148601+nfishel48@users.noreply.github.com>
2019-10-01 21:45:53 -0400

    added subtract method
    Bug with subtraction of negatives

--- Cosc 221/src/project3.java
+++ Cosc 221/src/project3.java
@@ -15,22 +15,22 @@ import java.util.*;
 import java.lang.Math;
 
 public class project3 {
-	
+
 	static String p3Input() {
 		System.out.print("Enter a Binary Number: ");
 		Scanner stdIn = new Scanner(System.in);
 		String input = stdIn.nextLine();
 		return input;
-		
+
 	}
-	
+
 	public static boolean reboo(String input) {
 		boolean negitive = false;
-		if (input.charAt(0) == '1') {//checks the sign of the number
+		if (input.charAt(0) == '1') {// checks the sign of the number
 			negitive = true;
 		}
 
-		return negitive; //returns if the number is positive or negitive
+		return negitive; // returns if the number is positive or negitive
 	}
 
 	public static String checkSign(String biNum) {
@@ -155,72 +155,67 @@ public class project3 {
 					back[count] = 1;
 					count++;
 				}
-			}//back[] now holds the reverse of the binary number if the number is negitive.
-			if(extreme == 128) {
-			System.out.println("The Binary version of "+decimalNum+" is:");
-			for (int j = 7; j >= 0; j--) { //prints
-				System.out.print(arr[j]);
-			}
-			}
-			else {
-			System.out.println();
-			count = 0;
-			int bcount = 0;
-			int carry = 0;
-			// while(count < 8) {
-			switch (back[bcount]) {// if the first number is zero add one 
-			case 0:
-				back[bcount] = 1;
-				carry = 1;
-				break;
-			case 1:
-				// System.out.println();
-				// System.out.println(back[bcount]);
-				back[bcount] = 0;
-				bcount++;
-				// System.out.println(back[bcount]);
-				back[bcount] = 1;
-
-				break;
-			}
-			// }
-			if (carry == 1) { // does the carry operation
-				while (count < 7) {
-					if (back[bcount] == 0) {
-						back[bcount] = 1;
-						bcount--;
-					} else {
-						back[bcount] = 1;
-						count = 7;
-					}
-					count++;
+			} // back[] now holds the reverse of the binary number if the number is negitive.
+			if (extreme == 128) {
+				System.out.println("The Binary version of " + decimalNum + " is:");
+				for (int j = 7; j >= 0; j--) { // prints
+					System.out.print(arr[j]);
 				}
-				System.out.println("The Binary version of "+decimalNum+" is:");
-			for (int j = 7; j >= 0; j--) { //prints
-				System.out.print(back[j]);
-			}
-			}
+			} else {
+				System.out.println();
+				count = 0;
+				int bcount = 0;
+				int carry = 0;
+				// while(count < 8) {
+				switch (back[bcount]) {// if the first number is zero add one
+				case 0:
+					back[bcount] = 1;
+					carry = 1;
+					break;
+				case 1:
+					// System.out.println();
+					// System.out.println(back[bcount]);
+					back[bcount] = 0;
+					bcount++;
+					// System.out.println(back[bcount]);
+					back[bcount] = 1;
+
+					break;
+				}
+				// }
+				if (carry == 1) { // does the carry operation
+					while (count < 7) {
+						if (back[bcount] == 0) {
+							back[bcount] = 1;
+							bcount--;
+						} else {
+							back[bcount] = 1;
+							count = 7;
+						}
+						count++;
+					}
+					System.out.println("The Binary version of " + decimalNum + " is:");
+					for (int j = 7; j >= 0; j--) { // prints
+						System.out.print(back[j]);
+					}
+				}
 			}
 		}
 
-		 else {
-			 System.out.println("The Binary version of "+decimalNum+" is:");
+		else {
+			System.out.println("The Binary version of " + decimalNum + " is:");
 
-			for (int j = 7; j >= 0; j--) {//prints
+			for (int j = 7; j >= 0; j--) {// prints
 				System.out.print(arr[j]);
 			}
 		}
-		
-		
 
 		System.out.println();
-		
-	
+
 	}
-	
 
 	public static void convertTodecimal(String input) {// this takes a binary number and converts the decimal
-		
+
 		boolean negitive = reboo(input);
 		input = checkSign(input);
 		String biNum = input;
@@ -244,8 +239,8 @@ public class project3 {
 	}
 
 	public static void menu() {
-		System.out.println("Main Menu");//This prints the main menu
-		System.out.println("Please enter a choice \n 1: Add two binary numbers\n 2: Convert binary to decimal");
+		System.out.println("Main Menu");// This prints the main menu
+		System.out.println("Please enter a choice \n 1: Add two binary numbers\n 2: Subtract two binary numbers");
 		int choice = readIntInput();
 		boolean done = false;
 		while (done != true) {
@@ -256,7 +251,7 @@ public class project3 {
 				break;
 
 			case 2:
-				//convertTodecimal();
+				subtract();
 				done = true;
 				break;
 			}
@@ -265,7 +260,7 @@ public class project3 {
 		again();
 	}
 
-	public static void again() {//This calls the main menu again or exits the program
+	public static void again() {// This calls the main menu again or exits the program
 		System.out.println("Press 1 if you would to return to the menu\n any other int will exit");
 		int choice = readIntInput();
 		if (choice == 1) {
@@ -273,60 +268,134 @@ public class project3 {
 		} else
 			System.exit(0);
 	}
-	
-	static void add () {
+
+	static void subtract() {
 		String b1 = p3Input();
 		convertTodecimal(b1);
-		String b2 = p3Input();//Get both numbers from the user
+		String b2 = p3Input();// Get both numbers from the user
 		convertTodecimal(b2);
 		int arr1[] = new int[8];
-		int arr2[] = new int[8];//arrays to store Binary numbers in
+		int arr2[] = new int[8];// arrays to store Binary numbers in
+		int back[] = new int[8];
 		int arr3[] = new int[8];
-		
-		
-		for(int i = 0; i<=7; i++) {
+
+		for (int i = 0; i <= 7; i++) {
 			arr1[i] = b1.charAt(i) - 48;
-			arr2[i] = b2.charAt(i) - 48;//Read through the the number placing single numbers into spots in arrays
+			arr2[i] = b2.charAt(i) - 48;// Read through the the number placing single numbers into spots in arrays
 		}
-		//loop through array to do binary additon with single digits
-		for(int j = 0; j<=7; j++) {//flip so does not overwrite
-			//System.out.println("arr1: "+arr1[j]+" arr2: "+arr2[j]);
-			// 1+1= 0 when no remainder
-			if(arr1[j] == 1 && arr2[j] == 1) {
+		int count = 0;
+		if (arr2[0] == 1) {// flip the number then subtract 1
+			while (count < 8) {
+				if (arr2[count] == 1) {
+					back[count] = 0;
+					count++;
+				} else {
+					back[count] = 1;
+					count++;
+				}
+			}
+			for (int y = 0; y <= 7; y++) {
+				arr2[y] = back[y];
+			}
+			for (int z = 7; z >= 0; z--) {// add one
+				if (arr2[z] == 0) {
+					arr2[z] = 1;
+				} else {
+					arr2[z] = 0;
+					if (z > 0) {
+						arr2[z - 1] = 1;
+					}
+				}
+			}
+
+		}
+
+		// loop through array to do binary additon with single digits
+		for (int j = 0; j <= 7; j++) {// flip so does not overwrite
+			// System.out.println("arr1: "+arr1[j]+" arr2: "+arr2[j]);
+			// 1-1= 0
+			if (arr1[j] == 1 && arr2[j] == 1) {
 				arr3[j] = 0;
-				if(j-1 > 0) {
-				arr3[j-1] = 1;
+			}
+
+			// 1-0 = 1
+			else if (arr1[j] == 1 && arr2[j] == 0) {
+				arr3[j] = 1;
+
+			}
+			// 0-1 = 1 with no remainder
+			else if (arr1[j] == 0 && arr2[j] == 1) {
+				arr3[j] = 0;
+				if (j - 1 >= 0) {
+					arr3[j - 1] = 1;
+				}
+			}
+			// 0+0 = 0
+			else {
+				arr3[j] = 0;
+			}
+		}
+		StringBuffer sb = new StringBuffer();
+		for (int c = 0; c <= 7; c++) {
+			sb.append(arr3[c]);
+		}
+		String fin = sb.toString();
+		System.out.println("The difference is " + fin);
+		convertTodecimal(fin);
+
+	}
+
+	static void add() {
+		String b1 = p3Input();
+		convertTodecimal(b1);
+		String b2 = p3Input();// Get both numbers from the user
+		convertTodecimal(b2);
+		int arr1[] = new int[8];
+		int arr2[] = new int[8];// arrays to store Binary numbers in
+		int arr3[] = new int[8];
+
+		for (int i = 0; i <= 7; i++) {
+			arr1[i] = b1.charAt(i) - 48;
+			arr2[i] = b2.charAt(i) - 48;// Read through the the number placing single numbers into spots in arrays
+		}
+		// loop through array to do binary additon with single digits
+		for (int j = 0; j <= 7; j++) {// flip so does not overwrite
+			// System.out.println("arr1: "+arr1[j]+" arr2: "+arr2[j]);
+			// 1+1= 0 when no remainder
+			if (arr1[j] == 1 && arr2[j] == 1) {
+				arr3[j] = 0;
+				if (j - 1 > 0) {
+					arr3[j - 1] = 1;
 				}
 			}
 			// 1+0 = 1 with no remainder
-			else if(arr1[j] == 1 && arr2[j] == 0) {
+			else if (arr1[j] == 1 && arr2[j] == 0) {
 				arr3[j] = 1;
-				
+
 			}
 			// 0+1 = 1 with no remainder
-			else if(arr1[j] == 0 && arr2[j] == 1) {
+			else if (arr1[j] == 0 && arr2[j] == 1) {
 				arr3[j] = 1;
 			}
 			// 0+0 = 0
 			else {
 				arr3[j] = 0;
 			}
-				
+
 		}
-		//for two negative
-		if(arr1[0] == 1 && arr2[0] == 1) {
+		// for two negative
+		if (arr1[0] == 1 && arr2[0] == 1) {
 			arr3[0] = 1;
 		}
 		StringBuffer sb = new StringBuffer();
-		for(int c = 0; c<=7; c++) {
+		for (int c = 0; c <= 7; c++) {
 			sb.append(arr3[c]);
 		}
 		String fin = sb.toString();
-		System.out.println("The sum is "+fin);
+		System.out.println("The sum is " + fin);
 		convertTodecimal(fin);
 	}
-	
-	
+
 	public static void main(String[] args) {
 		menu();
 	}
